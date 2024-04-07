59cd6b4e6ffc182827ab87c4d10e4a0cfce3bcac
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-04-06 21:24:33 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/*
+ *
+ * 1. Crear una clase Punto que permita representar puntos en un plano de un sistema de coordenadas
+ * cartesianas.
+ * Crear un programa que permita ingresar los datos de dos puntos y con ellos cree dos instancias
+ * de la clase Punto.
+ * A partir de las instancias creadas calcular y mostrar la distancia existente entre ambos puntos.
+ *
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
